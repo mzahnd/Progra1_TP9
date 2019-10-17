1fa5e12767c75cbf81625cde5f2d43d057793c65
mzahnd <>
2019-10-17 20:24:41 -0300

    First Commit - Testing...

---
+++ main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: martin
+ *
+ * Created on October 17, 2019, 8:21 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int
+main(int argc, char** argv)
+{
+
+    return (EXIT_SUCCESS);
+}
+
