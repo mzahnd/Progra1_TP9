13e9115ce266a62b7bb241203581da5113a8fa33
mzahnd <martin@wolf>
2019-10-18 21:07:07 -0300

    Test - Función Palíndromos FIXME: Agregar función con su nombre real

---
+++ tests/fpalindromos_test.c
@@ -0,0 +1,265 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * File:   fpalindromos_test.c
+ * Author: martin
+ *
+ * Created on October 17, 2019, 9:58 PM
+ */
+
+// ====== Librerías ======
+#include <stdio.h>
+#include <stdlib.h>
+
+// ====== Constantes y Macros ======
+
+// ====== Globales ======
+int test_total, test_pass, test_fail;
+
+// ====== Prototipos ======
+void
+check_ans(int esperado, int *recibido, char* testname);
+
+// Tests
+void
+test1(void);
+void
+test2(void);
+void
+test3(void);
+void
+test4(void);
+void
+test5(void);
+void
+test6(void);
+void
+test7(void);
+void
+test8(void);
+void
+test9(void);
+void
+test10(void);
+// ====== Funciones ======
+
+/*
+ * Simple C Test Suite
+ */
+
+void
+test1(void)
+{
+    printf("Test 1 (--Function Name--)\n");
+
+    int ans = -2, fargc = 2;
+    char *fargv[] = {"./ejecutable", "hola"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 1");
+}
+
+void
+test2(void)
+{
+    printf("Test 2 (--Function Name--)\n");
+
+    int ans = -2, fargc = 2;
+    char *fargv[] = {"./ejecutable", "abcba"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 2");
+}
+
+void
+test3(void)
+{
+    printf("Test 3 (--Function Name--)\n");
+
+    int ans = -2, fargc = 2;
+    char *fargv[] = {"./ejecutable", "ab-ba"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 3");
+}
+
+void
+test4(void)
+{
+    printf("Test 4 (--Function Name--)\n");
+
+    int ans = -2, fargc = 2;
+    char *fargv[] = {"./ejecutable", "-"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 4");
+}
+
+void
+test5(void)
+{
+    printf("Test 5 (--Function Name--)\n");
+
+    int ans = -2, fargc = 3;
+    char *fargv[] = {"./ejecutable", "abc", "cbc"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 5");
+}
+
+void
+test6(void)
+{
+    printf("Test 6 (--Function Name--)\n");
+
+    int ans = -2, fargc = 3;
+    char *fargv[] = {"./ejecutable", "abc", "."};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 6");
+}
+
+void
+test7(void)
+{
+    printf("Test 7 (--Function Name--)\n");
+
+    int ans = -2, fargc = 3;
+    char *fargv[] = {"./ejecutable", "ITBAabti", "CBC"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 7");
+}
+
+void
+test8(void)
+{
+    printf("Test 8 (--Function Name--)\n");
+
+    int ans = -2, fargc = 5;
+    char *fargv[] = {"./ejecutable", "ITBAabti", "CBC", "Anana",
+                     "Anita lava la tina"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 8");
+}
+
+void
+test9(void)
+{
+    printf("Test 9 (--Function Name--)\n");
+
+    int ans = -2, fargc = 2;
+    char *fargv[] = {"./ejecutable", "A man, a plan, a canal, Panama"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 9");
+}
+
+void
+test10(void)
+{
+    printf("Test 10 (--Function Name--)\n");
+
+    int ans = -2, fargc = 2;
+    char *fargv[] = {"./ejecutable", "A man. a plan; a canal, Panama"};
+
+    // 1 = Palíndromo ; 0 = No Palíndromo ; -1 = Error
+    //ans = funcion(fargc, fargv);
+
+    check_ans(0, &ans, "Test 10");
+}
+
+void
+check_ans(int esperado, int *recibido, char* testname)
+{
+    test_total++;
+    if(esperado != *recibido)
+    {
+        test_fail++;
+
+        printf("%%TEST_FAILED%% testname=%s message=Error, se esperaba %d ",
+               testname, esperado);
+        printf(" pero se recibió %d\n", *recibido);
+    }
+    else
+    {
+        test_pass++;
+    }
+}
+
+int
+main(int argc, char** argv)
+{
+    printf("%%SUITE_STARTING%% fpalindromos_test\n");
+    printf("%%SUITE_STARTED%%\n");
+
+    printf("%%TEST_STARTED%% \n");
+    test1();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test2();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test3();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test4();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test5();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test6();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test7();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test8();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test9();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TEST_STARTED%% \n");
+    test10();
+    printf("%%TEST_FINISHED%% \n");
+
+    printf("%%TESTS_PASSED%% %d \n", test_pass);
+    printf("%%TESTS_FAILED%% %d \n", test_fail);
+    printf("%%TESTS_TOTAL%% %d \n", test_total);
+    printf("%%SUITE_FINISHED%%\n");
+
+    return (EXIT_SUCCESS);
+}
